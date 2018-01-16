61e624ff6ad3ef60f792efe15eac4face04290f1
Arnaud Leherpeur <Arnaud@localhost.localdomain>
2018-01-16 15:01:46 +0100

    Ajouts de l'index.php

---
+++ server_master/index.php
@@ -0,0 +1,18 @@
+<?php
+     /*
+     PROJET: INNOVATION WEEK 2017 - NO NAME
+     MAKER: Arnaud LEHERPEUR (Martient)
+     DESCRIPTION: Service de modulation de calendrier en IOT.
+     */
+?>
+    
+<!DOCTYPE html PUBLIC>
+    <head>
+        <title>INNOVATION WEEK 2017 - SERVICE DE CALENDRIER</title>
+    </head>
+    <body>
+        
+    </body>
+    <footer>
+        
+    </footer>
